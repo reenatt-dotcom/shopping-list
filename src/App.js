a03d6ec13d10e21fc47e25992632c415a1279020
reenatt-dotcom <rtthomas@ucsc.edu>
2023-06-08 16:42:37 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newItem = event.target.item.value.trim();
+    if (newItem !== "") {
+      setItems([...items, newItem]);
+      event.target.reset();
+    }
+  }
+
+  function deleteItem(index) {
+    setItems(items.filter((_, i) => i !== index));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Project 4: Shopping List</h1>
+      <div className="container">
+        <div className="list">
+          <h2>Items To Buy</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="white">
+              <input type="text" name="item" placeholder="Add a new item" />
+              <button type="submit">Add</button>
+            </div>
+          </form>
+
+          <ul>
+            {items.map((item, index) => (
+              <li key={index}>
+                {item}
+                <button className="delete" onClick={() => deleteItem(index)}>
+                  x
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
